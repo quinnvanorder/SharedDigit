afc17a0b7881734f7852a582f7658d3506ca33c0
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-06-07 18:36:39 -0700

    Created framework and detailed challenge in main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+Write a method named hasSharedDigit with two parameters of type int
+each number should be within the range of 10 - 99. If either is not within range return false.
+The method should return true if there is a digit that appears in both numbers, such as 2 in 21 and 12, otherwise return false.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
